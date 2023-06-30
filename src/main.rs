79db5a17eea74b92620b35d465dffcb2c4a3ccb0
miyayamaM <pendulum.ngt.4010@gmail.com>
2023-06-30 21:38:31 +0900

    resolve conflict

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use std::fs::File;
+use std::io::{BufWriter, Write};
+
+fn main() -> Result<()> {
+    let url =
+        "https://raw.githubusercontent.com/beefsack/bgg-ranking-historicals/master/2023-06-29.csv";
+    let body = reqwest::blocking::get(url)?.text()?;
+
+    let csf_file = File::create("2023-06-29.csv")?;
+    let mut writer = BufWriter::new(csf_file);
+    writer.write(body.as_bytes())?;
+
+    Ok(())
 }
