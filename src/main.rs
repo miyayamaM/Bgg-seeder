1ae07451f82cdccaed5c2cc89386ace16dc9f4b0
miyayamaM <pendulum.ngt.4010@gmail.com>
2023-07-05 21:19:45 +0900

    feat: insert a record

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,38 @@
 use anyhow::Result;
+use sqlx::{Connection, MySqlConnection};
 use std::fs::File;
 use std::io::{BufWriter, Write};
 
-fn main() -> Result<()> {
+#[tokio::main]
+async fn main() -> Result<()> {
+    // GitHubからcsvを取得
     let url =
         "https://raw.githubusercontent.com/beefsack/bgg-ranking-historicals/master/2023-06-29.csv";
-    let body = reqwest::blocking::get(url)?.text()?;
+    let body = reqwest::get(url).await?.text().await?;
 
     let csf_file = File::create("2023-06-29.csv")?;
     let mut writer = BufWriter::new(csf_file);
     writer.write(body.as_bytes())?;
+
+    // DBと接続
+    let mut connection =
+        MySqlConnection::connect("mysql://root:@localhost:3306/bgg_seeder").await?;
+
+    sqlx::query(
+        r#"
+    INSERT INTO boardgames(
+        id,
+        name
+    )
+    VALUES(
+        1,
+        "SCOUT!"
+    );
+"#,
+    )
+    .execute(&mut connection)
+    .await?;
+    // csvを解析してDBへいれる
 
     Ok(())
 }
